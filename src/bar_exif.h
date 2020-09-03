724508f504fb0854ee9c7af6ba9699e4abe08430
rezso <rezso@rezso.net>
2020-09-03 03:49:55 +0200

    Fix build with gcc 10

--- src/bar_exif.h
+++ src/bar_exif.h
@@ -24,8 +24,8 @@ gint bar_exif_is_advanced(GtkWidget *bar);
 
 /* these are exposed for when duplication of the exif bar's text is needed */
 
-const gchar **bar_exif_key_list;
-const gint bar_exif_key_count;
+extern const gchar **bar_exif_key_list;
+extern const gint bar_exif_key_count;
 
 gchar *bar_exif_validate_text(gchar *text);
 
